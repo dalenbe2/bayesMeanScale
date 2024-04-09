6cb5ffc5a417b63901b205dbf7feaf660f017955
David Dalenberg <dalenbe2@gmail.com>
2024-04-09 16:15:48 -0400

    Created some print methods

---
+++ R/print.R
@@ -0,0 +1,19 @@
+
+print.bayes_mean_scale_pred <- function(x, ...){
+  
+  print(x$predTable, row.names=F, ...)
+  
+}
+
+print.bayes_mean_scale_marg <- function(x, ...){
+  
+  print(x$diffTable, row.names=F, ...)
+  
+}
+
+print.bayes_mean_scale_marg_compare <- function(x, ...){
+  
+  xNew <- as.data.frame(x)
+  print(xNew, row.names=F, ...)
+  
+}
