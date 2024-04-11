188342f04a45cbc7d94f52b4ebb961aab2308cd8
David Dalenberg <dalenbe2@gmail.com>
2024-04-11 15:44:04 -0400

    Add in warning check

--- tests/testthat/test-bayes-preds.R
+++ tests/testthat/test-bayes-preds.R
@@ -2,13 +2,21 @@
 
 test_that("make sure all configurations of bayesPredsF run without error", {
   
-  expect_no_error(bayesPredsF(logitModel2, at=list(educ=c(0, 12))))
-  expect_no_error(bayesPredsF(logitModel, at=list(educ=c(0, 12))))
-  expect_no_error(bayesPredsF(logitModel, at=list(educ=c(0, 12)), hdi_interval=F))
-  expect_no_error(bayesPredsF(logitModel, at=list(educ=c(0, 12)), at_means=T))
-  expect_no_error(bayesPredsF(logitModel, at=list(educ=c(0, 12)), hdi_interval=F, at_means=T))
+  expect_no_error(bayesPredsF(logitModel2, at=list(educ=c(0, 12)), n_draws=100))
+  expect_no_error(bayesPredsF(logitModel, at=list(educ=c(0, 12)), n_draws=100))
+  expect_no_error(bayesPredsF(logitModel, at=list(educ=c(0, 12)), hdi_interval=F, n_draws=100))
+  expect_no_error(bayesPredsF(logitModel, at=list(educ=c(0, 12)), at_means=T, n_draws=100))
+  expect_no_error(bayesPredsF(logitModel, at=list(educ=c(0, 12)), hdi_interval=F, at_means=T, n_draws=100))
   
 })
 
-
+test_that("make sure all configurations of bayesPredsF run without warning", {
+  
+  expect_no_warning(bayesPredsF(logitModel2, at=list(educ=c(0, 12)), n_draws=100))
+  expect_no_warning(bayesPredsF(logitModel, at=list(educ=c(0, 12)), n_draws=100))
+  expect_no_warning(bayesPredsF(logitModel, at=list(educ=c(0, 12)), hdi_interval=F, n_draws=100))
+  expect_no_warning(bayesPredsF(logitModel, at=list(educ=c(0, 12)), at_means=T, n_draws=100))
+  expect_no_warning(bayesPredsF(logitModel, at=list(educ=c(0, 12)), hdi_interval=F, at_means=T, n_draws=100))
+  
+})
 
