67b78280e5e93b2a831c8a31f5905827a4262405
David Dalenberg <dalenbe2@gmail.com>
2024-04-11 15:41:31 -0400

    Added in "count" to global variable to fix R CMD check note

--- R/global-vars.R
+++ R/global-vars.R
@@ -1,2 +1,2 @@
 
-if(getRversion() >= "2.15.1") utils::globalVariables(c(".", ".SD", "..atVars", ":=", "..atVarsNew", "..at_vars", "pred"))
+if(getRversion() >= "2.15.1") utils::globalVariables(c(".", ".SD", "..atVars", ":=", "..atVarsNew", "..at_vars", "pred", "count"))
