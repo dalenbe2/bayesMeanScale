f505fa0a040cd4290dd432daf70fafb264a00393
David Dalenberg <dalenbe2@gmail.com>
2024-04-11 15:44:58 -0400

    Added in warning check

--- tests/testthat/test-marg-compare.R
+++ tests/testthat/test-marg-compare.R
@@ -3,11 +3,19 @@ m1AMEInteraction <- bayesMargEffF(logitModel,
                                   marginal_effect = 'dist',
                                   start_value     = 64.041,
                                   end_value       = 21.117,
-                                  at              = list(educ=c(0, 5, 8)))
+                                  at              = list(educ=c(0, 5, 8)),
+                                  n_draws         = 100)
 
 test_that("test that bayesMargCompareF runs without error", {
   
   expect_no_error(bayesMargCompareF(m1AMEInteraction))
   expect_no_error(bayesMargCompareF(m1AMEInteraction, centrality='median'))
+  
+})
+
+test_that("test that bayesMargCompareF runs without warning", {
+  
+  expect_no_warning(bayesMargCompareF(m1AMEInteraction))
+  expect_no_warning(bayesMargCompareF(m1AMEInteraction, centrality='median'))
   
 })
