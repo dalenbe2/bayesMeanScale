d16b6d37c96b4f6f0d2f4bbc2629553d20ad0d40
David Dalenberg <dalenbe2@gmail.com>
2024-04-18 09:43:05 -0400

    Added in a couple count models for testing

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,6 @@
 
 library(rstanarm)
+library(tibble)
 
 data(wells)
 
@@ -25,6 +26,18 @@ crabs <- read.table("https://users.stat.ufl.edu/~aa/cat/data/Crabs.dat", header=
 poissonModel  <- stan_glm(sat ~ weight + width, data=crabs, family=poisson, refresh=0)
 negBinomModel <- stan_glm(sat ~ weight + width, data=crabs, family=neg_binomial_2, refresh=0)
 
+poissonData <- tibble(
+  x     = rnorm(2000, mean=3),
+  w     = rnorm(2000, mean=2),
+  log_y = .5 + .2*x - .2*w,
+  y     = rpois(2000, lambda=exp(log_y))
+)
+
+summary(poissonData$y)
+
+poissonModel2  <- stan_glm(y ~ x + w, data=poissonData, family=poisson, refresh=0)
+negBinomModel2 <- stan_glm(y ~ x + w, data=poissonData, family=neg_binomial_2, refresh=0)
+
 ## save the data for internal use ##
 
-usethis::use_data(logitModel, logitModel2, poissonModel, negBinomModel, internal=T, overwrite=T)
+usethis::use_data(logitModel, logitModel2, poissonModel, negBinomModel, poissonModel2, negBinomModel2, internal=T, overwrite=T)
