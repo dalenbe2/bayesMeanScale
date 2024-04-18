fc7252b8b368ebeae58a92bb7b5e4c1f6144516c
David Dalenberg <dalenbe2@gmail.com>
2024-04-18 09:44:55 -0400

    Changed number of posterior draws to default and fixed ame vs. mem comparison

--- tests/testthat/test-marg-effects.R
+++ tests/testthat/test-marg-effects.R
@@ -2,42 +2,38 @@
 
 test_that("make sure all configurations of bayesMargEffF run without error", {
   
-  expect_no_error(bayesMargEffF(logitModel2, marginal_effect='log(dist)', start_value=4, end_value=3, at=list(educ=c(0, 12)), n_draws=100))
-  expect_no_error(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), n_draws=100))
-  expect_no_error(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), hdi_interval=F, n_draws=100))
-  expect_no_error(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), at_means=T, n_draws=100))
-  expect_no_error(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), at_means=T, hdi_interval=F, n_draws=100))
+  expect_no_error(bayesMargEffF(logitModel2, marginal_effect='log(dist)', start_value=4, end_value=3, at=list(educ=c(0, 12))))
+  expect_no_error(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12))))
+  expect_no_error(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), hdi_interval=F))
+  expect_no_error(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), at_means=T))
+  expect_no_error(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), at_means=T, hdi_interval=F))
   
 })
 
 test_that("make sure all configurations of bayesMargEffF run without warning", {
   
-  expect_no_warning(bayesMargEffF(logitModel2, marginal_effect='log(dist)', start_value=4, end_value=3, at=list(educ=c(0, 12)), n_draws=100))
-  expect_no_warning(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), n_draws=100))
-  expect_no_warning(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), hdi_interval=F, n_draws=100))
-  expect_no_warning(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), at_means=T, n_draws=100))
-  expect_no_warning(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), at_means=T, hdi_interval=F, n_draws=100))
+  expect_no_warning(bayesMargEffF(logitModel2, marginal_effect='log(dist)', start_value=4, end_value=3, at=list(educ=c(0, 12))))
+  expect_no_warning(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12))))
+  expect_no_warning(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), hdi_interval=F))
+  expect_no_warning(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), at_means=T))
+  expect_no_warning(bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), at_means=T, hdi_interval=F))
   
 })
 
 ame <- bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), digits=6)$diffTable %>%
-  subset(., select=c(mean, lower, upper))
+  subset(., select=c(mean))
 
 mem <- bayesMargEffF(logitModel, marginal_effect='dist', start_value=50, end_value=20, at=list(educ=c(0, 12)), digits=6, at_means=T)$diffTable %>%
-  subset(., select=c(mean, lower, upper))
+  subset(., select=c(mean))
 
 diffs <- abs(ame-mem)
 
 test_that('make sure ame and mem are close but not exactly the same', {
-  
-  for(i in nrow(diffs)){
-    for(j in ncol(diffs)){
       
-      expect_gt(diffs[i, j], 0)
-      expect_lt(diffs[i, j], ame[i, j]*1.06 - ame[i, j])
+      expect_gt(diffs[1,], 0)
+      expect_gt(diffs[2,], 0)
+      expect_lt(diffs[1,], abs(ame[1,]*1.1 - ame[1,]))
+      expect_lt(diffs[2,], abs(ame[2,]*1.1 - ame[2,]))
       
-    }
-  }
-
-  
 })
+
