bc29f7bb2147a5c63b9d4b3681e8924b3f184ed5
David Dalenberg <dalenbe2@gmail.com>
2024-04-20 13:22:19 -0400

    Added '::' syntax

--- R/mean-diff.R
+++ R/mean-diff.R
@@ -6,12 +6,12 @@ meanDiffF <- function(pred_start, pred_end, model_data, marg_list, at, at_means,
     atVars <- names(expand.grid(at))
 
     predDiffOrg <- pred_start - pred_end %>%
-      as.data.table()
+      data.table::as.data.table()
 
     predDiff <- predDiffOrg %>%
       cbind(model_data[, ..atVars]) %>%
       .[, lapply(.SD, mean), by=atVars, .SDcols=!atVars] %>%
-      melt(id.vars       = atVars,
+      data.table::melt(id.vars       = atVars,
            variable.name = 'which_diff',
            value.name    = 'diff') %>%
       .[, !"which_diff"] %>%
@@ -22,7 +22,7 @@ meanDiffF <- function(pred_start, pred_end, model_data, marg_list, at, at_means,
   
   if(is.null(at) & at_means==F){
 
-    predDiff <- data.table(diff        = colMeans(pred_start - pred_end),
+    predDiff <- data.table::data.table(diff        = colMeans(pred_start - pred_end),
                            comparison  = paste0(marg_list$start[[i]], " vs. ", marg_list$end[[i]]),
                            marg_effect = marg_list$marg[[i]])
 
@@ -34,11 +34,11 @@ meanDiffF <- function(pred_start, pred_end, model_data, marg_list, at, at_means,
     atValues <- expand.grid(at)
     
     predDiffOrg <- pred_start - pred_end %>%
-      as.data.table()
+      data.table::as.data.table()
     
     predDiff <- predDiffOrg %>%
       cbind(atValues) %>%
-      melt(id.vars       = atVars,
+      data.table::melt(id.vars       = atVars,
            variable.name = 'which_diff',
            value.name    = 'diff') %>%
       .[, !"which_diff"] %>%
