2b07d5dfdb451ce1f843ff4652fdbc7416a7d3cc
David Dalenberg <dalenbe2@gmail.com>
2024-04-28 09:29:07 -0400

    Initial commit

---
+++ R/bayes-ordinal-preds.R
@@ -0,0 +1,73 @@
+
+bayesOrdinalPredsF <- function(model, at, n_draws=2000, ci=.95, hdi_interval=TRUE, centrality='mean', digits=4, at_means=FALSE){
+
+  predOrdinalErrorCheckF(model      = model,
+                         at         = at,
+                         centrality = centrality)
+  
+  # modify the model formula if there's an offset #
+  
+  formulaNoOffsets <- modifyFormulaF(model=model)
+
+  # tack on the grouping variables #
+  
+  atValues  <- expand.grid(at)
+  atVars    <- names(atValues)
+  
+  modelData <- modelDataOrg %>%
+    na.omit() %>%
+    .[, !(colnames(.) %in% atVars), drop=F] %>%
+    merge(atValues, all=T) %>%
+    data.table::setDT()
+
+  # do some checks for data integrity #
+  
+  newData <- levelsPrepF(data          = modelData,
+                         at            = atValues,
+                         original_data = modelDataOrg)
+  
+  dataCheckF(new_data    = newData,
+             model_frame = modelDataOrg)
+
+  # get the ordered outcomes for the response variable #
+
+  yOutcomes <- levels(model$y)
+  
+  # get the draws #
+  
+  draws <- sample(1:nrow(posterior::as_draws_df(model)), size=n_draws, replace=T)
+
+  # get the predictions #
+  
+  preds <- meanPredOrdinalF(model,
+                            new_data    = newData,
+                            at          = at,
+                            draws       = draws,
+                            y_outcomes  = yOutcomes,
+                            new_formula = formulaNoOffsets,
+                            at_means    = at_means)
+  
+  # get the results table #
+  
+  predTable <- predOrdinalTableF(preds        = preds,
+                                 model_data   = modelData,
+                                 at_vars      = atVars,
+                                 at_values    = atValues,
+                                 hdi_interval = hdi_interval,
+                                 centrality   = centrality,
+                                 digits       = digits,
+                                 ci           = ci,
+                                 at_means     = at_means)
+
+  # output #
+  
+  predList <- structure(list(predDraws = preds,
+                             predTable = as.data.frame(predTable)),
+                        class = c("bayes_mean_scale_pred", "list"),
+                        scale = "mean")
+  
+  
+  return(predList)
+
+}
+
