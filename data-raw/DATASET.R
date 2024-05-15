35bfb4d750f34278c422ce9d18a9c02e5a964562
David Dalenberg <dalenbe2@gmail.com>
2024-05-15 06:44:45 -0400

    only run if rstanarm and tibble are installed

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 
-library(rstanarm)
-library(tibble)
+
+if(require(rstanarm) & require(tibble)){
 
 data(wells)
 
@@ -18,15 +18,15 @@ for(i in 1:10){
 
 summary(modelData)
 
-logitModel  <- stan_glm(switch ~ dist*educ + arsenic + I(arsenic^2) + assoc, data=modelData, family=binomial, refresh=0)
-logitModel2 <- stan_glm(switch ~ log(dist) + educ + arsenic + I(arsenic^2) + as.factor(assoc), data=modelData, family=binomial, refresh=0)
+logitModel  <- rstanarm::stan_glm(switch ~ dist*educ + arsenic + I(arsenic^2) + assoc, data=modelData, family=binomial, refresh=0)
+logitModel2 <- rstanarm::stan_glm(switch ~ log(dist) + educ + arsenic + I(arsenic^2) + as.factor(assoc), data=modelData, family=binomial, refresh=0)
 
 crabs <- read.table("https://users.stat.ufl.edu/~aa/cat/data/Crabs.dat", header=T)
 
-poissonModel  <- stan_glm(sat ~ weight + width, data=crabs, family=poisson, refresh=0)
-negBinomModel <- stan_glm(sat ~ weight + width, data=crabs, family=neg_binomial_2, refresh=0)
+poissonModel  <- rstanarm::stan_glm(sat ~ weight + width, data=crabs, family=poisson, refresh=0)
+negBinomModel <- rstanarm::stan_glm(sat ~ weight + width, data=crabs, family=neg_binomial_2, refresh=0)
 
-poissonData <- tibble(
+poissonData <- tibble::tibble(
   x     = rnorm(2000, mean=3),
   w     = rnorm(2000, mean=2),
   log_y = .5 + .2*x - .2*w,
@@ -35,9 +35,11 @@ poissonData <- tibble(
 
 summary(poissonData$y)
 
-poissonModel2  <- stan_glm(y ~ x + w, data=poissonData, family=poisson, refresh=0)
-negBinomModel2 <- stan_glm(y ~ x + w, data=poissonData, family=neg_binomial_2, refresh=0)
+poissonModel2  <- rstanarm::stan_glm(y ~ x + w, data=poissonData, family=poisson, refresh=0)
+negBinomModel2 <- rstanarm::stan_glm(y ~ x + w, data=poissonData, family=neg_binomial_2, refresh=0)
 
 ## save the data for internal use ##
 
 usethis::use_data(logitModel, logitModel2, poissonModel, negBinomModel, poissonModel2, negBinomModel2, internal=T, overwrite=T)
+
+}
