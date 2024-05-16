8ac4e2b6ff86923514edc48782b55c9828306545
David Dalenberg <dalenbe2@gmail.com>
2024-05-16 06:34:43 -0400

    Move all code into test_that()

--- tests/testthat/test-data-check.R
+++ tests/testthat/test-data-check.R
@@ -1,20 +1,22 @@
 
-newData        <- data.frame(x = factor(c('A', 'B')))
-modelFrameData <- data.frame(x = factor(c('A', 'A')))
-
-newData1        <- data.frame(x = factor(c('A', 'B')))
-modelFrameData1 <- data.frame(x = c('A', 'A'))
-
-newData2        <- data.frame(x = c('A', 'B'))
-modelFrameData2 <- data.frame(x = factor(c('A', 'A')))
-
-newData3        <- data.frame(x = c('A', 'B'))
-modelFrameData3 <- data.frame(x = c('A', 'A'))
-
-newData4        <- data.frame(x = c('a', 'b'))
-modelFrameData4 <- data.frame(x = c(1,2))
 
 test_that("make sure dataCheckF detects errors", {
+  
+  newData        <- data.frame(x = factor(c('A', 'B')))
+  modelFrameData <- data.frame(x = factor(c('A', 'A')))
+  
+  newData1        <- data.frame(x = factor(c('A', 'B')))
+  modelFrameData1 <- data.frame(x = c('A', 'A'))
+  
+  newData2        <- data.frame(x = c('A', 'B'))
+  modelFrameData2 <- data.frame(x = factor(c('A', 'A')))
+  
+  newData3        <- data.frame(x = c('A', 'B'))
+  modelFrameData3 <- data.frame(x = c('A', 'A'))
+  
+  newData4        <- data.frame(x = c('a', 'b'))
+  modelFrameData4 <- data.frame(x = c(1,2))
+  
   expect_error(dataCheckF(newData, modelFrameData), regexp="There's a factor level mismatch in or or more of the model frame columns!")
   expect_error(dataCheckF(newData1, modelFrameData1), regexp="You specified the values wrong for some character/factor variable!")
   expect_error(dataCheckF(newData2, modelFrameData2), regexp="You specified the values wrong for some character/factor variable!")
--- tests/testthat/test-levels-prep.R
+++ tests/testthat/test-levels-prep.R
@@ -1,8 +1,9 @@
 
-newData      <- data.frame(a = c(1,2,3))
-originalData <- data.frame(a = c('a', 'b', 'c'))
 
 test_that("make sure levelsPrepF is working correctly", {
+  
+  newData      <- data.frame(a = c(1,2,3))
+  originalData <- data.frame(a = c('a', 'b', 'c'))
 
   expect_error(levelsPrepF(newData, newData, originalData), regexp="You supplied numeric values for a character/factor variable!")
 
