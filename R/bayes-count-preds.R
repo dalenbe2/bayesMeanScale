08054f6230d28b39e0e85a374f6db814f6d95798
David Dalenberg <dalenbe2@gmail.com>
2026-05-07 13:55:53 -0400

    add in s3 methods

--- R/bayes-count-preds.R
+++ R/bayes-count-preds.R
@@ -1,21 +1,25 @@
 
-bayesCountPredsF <- function(model, counts, at, n_draws=2000, ci=.95, hdi_interval=TRUE, centrality='mean', digits=4, at_means=FALSE, data_slice='full'){
+bayesCountPredsF <- function(x, ...){
+  UseMethod("bayesCountPredsF")
+}
 
-  countPredsErrorCheckF(model      = model,
+bayesCountPredsF.stanreg <- function(x, counts, at, n_draws=2000, ci=.95, hdi_interval=TRUE, centrality='mean', digits=4, at_means=FALSE, data_slice='full', ...){
+
+  countPredsErrorCheckF(model      = x,
                         counts     = counts,
                         at         = at,
                         centrality = centrality)
 
   # modify the model formula if there's an offset #
 
-  formulaNoOffsets <- modifyFormulaF(model=model)
+  formulaNoOffsets <- modifyFormulaF(model=x)
 
   # get the model data #
   
-  modelDataOrg <- model$data %>%
+  modelDataOrg <- x$data %>%
     .[, colnames(.) %in% all.vars(formulaNoOffsets), drop=F] %>%
-    .[row.names(model$model),] %>%
-    {if(!is.null(model$offset)) cbind(., offset=model$offset) else .}
+    .[row.names(x$model),] %>%
+    {if(!is.null(x$offset)) cbind(., offset=x$offset) else .}
 
   # prepare 'at' values and 'at' names #
   
@@ -47,11 +51,11 @@ bayesCountPredsF <- function(model, counts, at, n_draws=2000, ci=.95, hdi_interv
 
   # get the draws #
   
-  draws <- sample(1:nrow(posterior::as_draws_df(model)), size=n_draws, replace=T)
+  draws <- sample(1:nrow(posterior::as_draws_df(x)), size=n_draws, replace=T)
   
   # get the predictions #
 
-  preds <- meanCountPredF(model,
+  preds <- meanCountPredF(x,
                           new_data    = newData,
                           counts      = counts,
                           at          = at,
@@ -74,6 +78,89 @@ bayesCountPredsF <- function(model, counts, at, n_draws=2000, ci=.95, hdi_interv
 
   # output #
 
+  predList <- structure(list(predDraws = preds,
+                             predTable = as.data.frame(predTable)),
+                        class        = c("bayesmeanscale_pred", "list"), 
+                        response     = "count_probability", 
+                        at           = at, 
+                        at_means     = at_means, 
+                        n_draws      = n_draws, 
+                        ci           = ci, 
+                        hdi_interval = hdi_interval)
+  
+  
+  return(predList)
+  
+}
+
+
+bayesCountPredsF.data.frame <- function(x, model_data, model_formula, link_function, model_family, counts, at, model_offset=NULL, n_draws=2000, ci=.95, hdi_interval=TRUE, centrality='mean', digits=4, at_means=FALSE, data_slice='full', ...){
+  
+  # get the model data #
+  
+  modelDataOrg <- model_data %>%
+    .[, colnames(.) %in% all.vars(model_formula), drop=F]
+  
+  # prepare 'at' values and 'at' names #
+  
+  atValues  <- expand.grid(at)
+  atVars    <- names(at)
+  
+  # check if there are any variables left to average over #
+  
+  if(ncol(modelDataOrg[, !(colnames(modelDataOrg) %in% c(atVars, "offset")), drop=F])==1){
+    data_slice <- 1
+  }
+  
+  # tack on the grouping variables #
+  
+  modelData <- modelDataOrg %>%
+    .[, !(colnames(.) %in% atVars), drop=F] %>%
+    {if(data_slice=="full") . else .[sample(1:nrow(modelDataOrg), size=data_slice, replace=T), colnames(.), drop=F]} %>%
+    merge(atValues, all=T) %>%
+    data.table::setDT()
+  
+  # do some checks for data integrity #
+  
+  newData <- levelsPrepF(data          = modelData,
+                         at            = atValues,
+                         original_data = modelDataOrg)
+  
+  dataCheckF(new_data    = newData,
+             model_frame = modelDataOrg)
+  
+  # get the draws #
+  
+  draws <- sample(1:nrow(x), size=n_draws, replace=T)
+  
+  # get the predictions #
+  
+  preds <- meanCountPredDFMethodF(x             = x,
+                                  new_data      = newData,
+                                  counts        = counts,
+                                  at            = at,
+                                  draws         = draws,
+                                  new_formula   = model_formula,
+                                  model_offset  = model_offset,
+                                  at_means      = at_means,
+                                  link_function = link_function,
+                                  model_family  = model_family)
+  
+  # get the results table #
+  
+  predTable <- countPredTableF(preds        = preds,
+                               model_data   = modelData,
+                               counts       = counts,
+                               at_vars      = atVars,
+                               at_values    = atValues,
+                               hdi_interval = hdi_interval,
+                               centrality   = centrality,
+                               digits       = digits,
+                               ci           = ci,
+                               at_means     = at_means)
+  
+  # output #
+  
   predList <- structure(list(predDraws = preds,
                              predTable = as.data.frame(predTable)),
                         class        = c("bayesmeanscale_pred", "list"), 
